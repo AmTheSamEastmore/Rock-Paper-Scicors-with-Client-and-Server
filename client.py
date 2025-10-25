733acfaaa967a3804cfc59d7921d11af8af47375
laptop <samvanoostveen@gmail.com>
2025-10-25 16:00:49 -0300

    You can now put a username when sending in the option. Version 1.0.1

--- client.py
+++ client.py
@@ -9,7 +9,20 @@ def send(message:str):
     with sk.socket(sk.AF_INET, sk.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         s.sendall(message.encode())
-        print(s.recv(1024).decode())
+        port = eval(s.recv(1024).decode())[0]
+        host = eval(s.recv(1024).decode())[1]
+    with sk.socket(sk.AF_INET, sk.SOCK_STREAM)  as s:
+        s.bind((host, port))
+        s.listen()
+        while 1:
+            con, adr = s.accept() # conetion, adrress
+            with con:
+                while 1:
+                    data = con.recv(1024)
+                    if not data:
+                        break
+                    else:
+                        pass
 
 class App(CTk):
     def __init__(self):
@@ -18,22 +31,36 @@ class App(CTk):
         set_default_color_theme('green')
         self.title('Rock, Paper, Scisors')
         self.geometry('700x450')
+    def press(self, option):
+        self.nick = nick_input.get()
+        if self.nick != '':
+            alert.config(text=send(f'{[self.nick, option]}'))
+            self.after(10000, lambda: alert.config(text=''))
 
 root = App()
 
+title = CTkLabel(root, text='Pick a Nickname and an Option', font=CTkFont('Arial', 22, 'bold'))
+title.place(x=0, y=0)
+
+nick_input = CTkEntry(root)
+nick_input.place(x=0, y=35)
+
 rockimg = Image.open('./assets/rock.png')
 rock_img_large = CTkImage(dark_image=rockimg, size=(100, 100))
-rock = CTkButton(root, height=100, width=100, image=rock_img_large, text='')
+rock = CTkButton(root, height=100, width=100, image=rock_img_large, text='', command=lambda: root.press('1'))
 rock.place(x=110, y=130)
 
 paperimg = Image.open('./assets/paper.png')
 paper_img_large = CTkImage(dark_image=paperimg, size=(100, 100))
-paper = CTkButton(root, height=100, width=100, image=paper_img_large, text='')
+paper = CTkButton(root, height=100, width=100, image=paper_img_large, text='', command=lambda: root.press('2'))
 paper.place(x=280, y=130)
 
 scisorsimg = Image.open('./assets/scisors.png')
 scisors_img_large = CTkImage(dark_image=scisorsimg, size=(100, 100))
-scisors = CTkButton(root, height=100, width=100, image=scisors_img_large, text='')
+scisors = CTkButton(root, height=100, width=100, image=scisors_img_large, text='', command=lambda: root.press('3'))
 scisors.place(x=450, y=130)
+
+alert = CTkLabel(root, text='', font=CTkFont('Arial', 22, 'bold'))
+alert.place(x=90, y=330)
 
 root.mainloop()
--- server.py
+++ server.py
@@ -10,9 +10,10 @@ def log(adress, data):
         x.write(f'({adress}, "{data}")\n')
 
 class Play:
-    def __init__(self, player, choice):
+    def __init__(self, player, choice, name):
         self.player = player
         self.choice = choice
+        self.name = name
 
 class Game:
     def __init__(self):
@@ -27,21 +28,21 @@ class Game:
             if self.self[1].choice == 'Rock':
                 print('Tie')
             if self.self[1].choice == 'Paper':
-                print(f'{self.self[1].player} Won')
+                print(f'{self.self[1].name} at {self.self[1].player} Won')
             if self.self[1].choice == 'Scicors':
-                print(f'{self.self[0].player} Won')
+                print(f'{self.self[1].name} at {self.self[1].player} Won')
         elif self.self[0].choice == 'Paper':
             if self.self[1].choice == 'Rock':
-                print(f'{self.self[0].player} Won')
+                print(f'{self.self[1].name} at {self.self[1].player} Won')
             if self.self[1].choice == 'Paper':
                 print('Tie')
             if self.self[1].choice == 'Scicors':
-                print(f'{self.self[1].player} Won')
+                print(f'{self.self[1].name} at {self.self[1].player} Won')
         elif self.self[0].choice == 'Scicors':
             if self.self[1].choice == 'Rock':
-                print(f'{self.self[1].player} Won')
+                print(f'{self.self[1].name} at {self.self[1].player} Won')
             if self.self[1].choice == 'Paper':
-                print(f'{self.self[0].player} Won')
+                print(f'{self.self[1].name} at {self.self[1].player} Won')
             if self.self[1].choice == 'Scicors':
                 print('Tie')
 
@@ -58,14 +59,15 @@ with sk.socket(sk.AF_INET, sk.SOCK_STREAM)  as s:
                 if not data:
                     break
                 else:
-                    log(adr, data.decode())
-                    if data.decode() == '1':
-                        print(f'{adr} chose Rock')
-                        game.add(Play(adr, 'Rock'))
-                    elif data.decode() == '2':
-                        print(f'{adr} chose Paper')
-                        game.add(Play(adr, 'Paper'))
-                    elif data.decode() == '3':
-                        print(f'{adr} chose Scicors')
-                        game.add(Play(adr, 'Rock'))
-                con.sendall(b'Recived messages')
+                    message = eval(data.decode())
+                    log(adr, message)
+                    if message[1] == '1':
+                        print(f'{message[0]} at {adr} chose Rock')
+                        game.add(Play(adr, 'Rock', message[0]))
+                    elif message[1] == '2':
+                        print(f'{message[0]} at {adr} chose Paper')
+                        game.add(Play(adr, 'Paper', message[0]))
+                    elif message[1] == '3':
+                        print(f'{message[0]} at {adr} chose Scicors')
+                        game.add(Play(adr, 'Rock', message[0]))
+                con.sendall(f'[{adr}, {con}]'.encode())
